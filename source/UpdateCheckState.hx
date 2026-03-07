453fe50fe234d1f0d27f34e1c5e9c89ec7cee5a1
chicken-nugget1104 <wakeywakeytothenewday@gmail.com>
2026-03-07 01:28:45 -0500

    updated to 0.2.0.1
    update checker, yay!

---
+++ source/UpdateCheckState.hx
@@ -0,0 +1,41 @@
+package;
+
+import flixel.FlxG;
+import lime.app.Application;
+import haxe.Http;
+import sys.io.Process;
+
+class UpdateCheckState extends MusicBeatState
+{
+    var latestVersion:String = "";
+    var currentVersion:String = "0.2.0.1";
+
+    override function create()
+    {
+        var url = "https://raw.githubusercontent.com/wakeywakeynow/RETROENGINEFNF/refs/heads/main/retrover.txt";
+        var http = new Http(url);
+
+        http.onData = function(response:String)
+        {
+            latestVersion = response;
+            if (latestVersion != currentVersion)
+            {
+                FlxG.openURL("https://gamebanana.com/tools/22083");
+                Sys.exit(0);
+            }
+            else
+            {
+                FlxG.switchState(new TitleState());
+            }
+        }
+
+        http.onError = function(err:String)
+        {
+            FlxG.switchState(new TitleState());
+        }
+
+        http.request();
+
+        super.create();
+    }
+}
