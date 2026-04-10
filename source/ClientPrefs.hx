bad0af5130aa15ae20b885f089b78c8bc8a7d31a
chicken-nugget1104 <wakeywakeytothenewday@gmail.com>
2026-04-09 22:44:58 -0400

    better FPS part 2

--- source/ClientPrefs.hx
+++ source/ClientPrefs.hx
@@ -5,16 +5,20 @@ import flixel.FlxG;
 class ClientPrefs
 {
 	public static var lowQuality:Bool = false;
+	public static var showFPS:Bool = true;
 
 	public static function load():Void
 	{
 		if (FlxG.save.data.lowQuality != null)
 			lowQuality = FlxG.save.data.lowQuality;
+		if (FlxG.save.data.showFPS != null)
+			showFPS = FlxG.save.data.showFPS;
 	}
 
 	public static function save():Void
 	{
 		FlxG.save.data.lowQuality = lowQuality;
+		FlxG.save.data.showFPS = showFPS;
 		FlxG.save.flush();
 	}
 }
--- source/RFPS.hx
+++ source/RFPS.hx
@@ -2,22 +2,27 @@ package;
 
 import openfl.display.Sprite;
 import openfl.events.Event;
+import openfl.events.KeyboardEvent;
 import openfl.text.TextField;
 import openfl.text.TextFormat;
 import openfl.Lib;
-import flixel.FlxG;
-import flixel.FlxBasic;
 import openfl.system.System;
+import flixel.FlxG;
 
 class RFPS extends Sprite
 {
 	var tf:TextField;
+	var fmt:TextFormat;
 
 	var frames:Int = 0;
 	var lastTime:Float = 0;
 
 	var fps:Int = 0;
 	var memory:Float = 0;
+
+	#if debug
+	var peakMemory:Float = 0;
+	#end
 
 	public function new(x:Int = 10, y:Int = 10, color:Int = 0xFFFFFF)
 	{
@@ -26,20 +31,35 @@ class RFPS extends Sprite
 		this.x = x;
 		this.y = y;
 
+		fmt = new TextFormat("_sans", 14, color);
+
 		tf = new TextField();
-		tf.defaultTextFormat = new TextFormat("_sans", 14, color);
+		tf.defaultTextFormat = fmt;
 		tf.width = 300;
-		tf.height = 100;
+		tf.height = 60;
 		tf.selectable = false;
 
 		addChild(tf);
 
 		lastTime = Lib.getTimer();
 
-		addEventListener(Event.ENTER_FRAME, update);
+		addEventListener(Event.ENTER_FRAME, onEnterFrame);
+		Lib.current.stage.addEventListener(KeyboardEvent.KEY_DOWN, onKeyDown);
+
+		visible = ClientPrefs.showFPS;
 	}
 
-	function update(e:Event)
+	function onKeyDown(e:KeyboardEvent)
+	{
+		if (e.keyCode == 114) // should be F3
+		{
+			visible = !visible;
+			ClientPrefs.showFPS = visible;
+			ClientPrefs.save();
+		}
+	}
+
+	function onEnterFrame(e:Event)
 	{
 		frames++;
 
@@ -51,8 +71,12 @@ class RFPS extends Sprite
 			frames = 0;
 			lastTime = now;
 
-			// MB
 			memory = System.totalMemory / 1024 / 1024;
+
+			#if debug
+			if (memory > peakMemory)
+				peakMemory = memory;
+			#end
 
 			updateText();
 		}
@@ -60,9 +84,33 @@ class RFPS extends Sprite
 
 	function updateText()
 	{
-		var text = "FPS: " + fps + "\n";
-		text += "MEM: " + Std.int(memory) + " MB";
+		var targetFPS:Int = FlxG.updateFramerate;
 
-		tf.text = text;
+		var fpsColor:Int;
+		if (fps >= Std.int(targetFPS * 0.9))
+			fpsColor = 0x00FF66;
+		else if (fps >= Std.int(targetFPS * 0.6))
+			fpsColor = 0xFFCC00;
+		else
+			fpsColor = 0xFF4444;
+
+		var fpsStr = "FPS: " + fps;
+		var memStr = "MEM: " + Std.int(memory) + " MB";
+
+		tf.text = fpsStr + "\n" + memStr;
+
+		#if debug
+		tf.appendText("\nPEAK: " + Std.int(peakMemory) + " MB");
+		#end
+
+		// base color!
+		tf.setTextFormat(new TextFormat("_sans", 14, 0xAAAAAA));
+
+		var fpsValueStart = fpsStr.indexOf(Std.string(fps));
+		tf.setTextFormat(
+			new TextFormat("_sans", 14, fpsColor),
+			fpsValueStart,
+			fpsValueStart + Std.string(fps).length
+		);
 	}
 }
