1db2414be1dfc51d277c145ee8e0613801f9be5d
chicken-nugget1104 <wakeywakeytothenewday@gmail.com>
2026-04-10 14:29:39 -0400

    how did i forgor these

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -121,6 +121,7 @@ class StoryMenuState extends MusicBeatState
 		leftArrow.animation.addByPrefix('idle', "arrow left");
 		leftArrow.animation.addByPrefix('press', "arrow push left");
 		leftArrow.animation.play('idle');
+		leftArrow.antialiasing = !ClientPrefs.lowQuality;
 		difficultySelectors.add(leftArrow);
 
 		sprDifficulty = new FlxSprite(leftArrow.x + 130, leftArrow.y);
@@ -129,6 +130,7 @@ class StoryMenuState extends MusicBeatState
 		sprDifficulty.animation.addByPrefix('normal', 'NORMAL');
 		sprDifficulty.animation.addByPrefix('hard', 'HARD');
 		sprDifficulty.animation.play('easy');
+		sprDifficulty.antialiasing = !ClientPrefs.lowQuality;
 		changeDifficulty();
 
 		difficultySelectors.add(sprDifficulty);
@@ -138,6 +140,7 @@ class StoryMenuState extends MusicBeatState
 		rightArrow.animation.addByPrefix('idle', 'arrow right');
 		rightArrow.animation.addByPrefix('press', "arrow push right", 24, false);
 		rightArrow.animation.play('idle');
+		rightArrow.antialiasing = !ClientPrefs.lowQuality;
 		difficultySelectors.add(rightArrow);
 
 		add(yellowBG);
